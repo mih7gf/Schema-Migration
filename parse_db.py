0735577ebcc7142efdf338d518ef8b3f9deaf74c
mih7gf <mih7gf@virginia.edu>
2017-07-07 17:28:41 -0400

    Parser Completed

--- parse_db.py
+++ parse_db.py
@@ -1,5 +1,6 @@
 import sqlite3
-from ODM1_datavalue_objects import *
+from ODM1_table_objects import *
+from migrate_db import *
 
 sqlite_file = '../hampt_rd_data.sqlite'
 conn = sqlite3.connect(sqlite_file)
@@ -12,10 +13,7 @@ tables = [ #[table_name, pk_column_name]
 	['variables', 'VariableID']
 ]
 
-dval_objs = []
-QCL_objs = []
-site_objs =[]
-variable_objs = []
+
 
 def get_all_rows(table_name):
 	c.execute('SELECT * FROM {tn}'.format(tn=table_name))
@@ -33,49 +31,64 @@ def get_row_by_pk(table_name, pk_name, row):
 	return one_row
 
 def parse_small_tables():
-	print "Parsing QCL..."
+	print "Fetching QCL..."
 	QCL_rows = get_all_rows(tables[1][0])	
+	print "Migrating QCL..."
 	for row in QCL_rows:
-		QCL_objs.append(QualityControlLevel(row))
+		# QCL_objs.append(QualityControlLevel(row))
+		migrate_QCL(QualityControlLevel(row))
 	print "Done\n"
 	
-	print "Parsing Sites..."
+	print "Fetching Sites..."
 	site_rows = get_all_rows(tables[2][0])
+	print "Migrating Sites..."
 	for row in site_rows:
-		site_objs.append(Site(row))
+		# site_objs.append(Site(row))
+		migrate_site(Site(row))
 	print "Done\n"
 
-	print "Parsing Variables..."
+	print "Fetching Variables..."
 	variable_rows = get_all_rows(tables[3][0])
+	print "Migrating Variables..."
 	for row in variable_rows:
-		variable_objs.append(Variable(row))
+		# variable_objs.append(Variable(row))
+		migrate_variable(Variable(row))
 	print "Done\n"
 	return
 
 def buffered_parse_large_table():
-	print "Parsing Datavalues..."
+	
 	num_rows = int(c.execute('SELECT * FROM {tn} ORDER BY ROWID DESC LIMIT 1'.format(tn=tables[0][0])).fetchone()[0])
-	num_rows = 5000023
-	buffer_size = 1000000
+	# num_rows = 25000000
+	buffer_size = 100000
 	start = 1
+	print "Fetching & Migrating Datavalues..."
 
 	while start < num_rows+1:
+
 		if start+buffer_size <= num_rows:
 			end = start+buffer_size
 		else:
 			end = num_rows+1
 
 		rows = get_rows_in_range(tables[0][0], tables[0][1], start, end)
-		dval_objs = []
-		for r in rows:
-			dval_objs.append(r)
-		print start,"to",end,": Done"
+		# dval_objs = []
+		for row in rows:
+			# dval_objs.append(Datavalue(row))
+			migrate_datavalue(Datavalue(row))
+		print start,"to",end,"("+str(100*end/num_rows)+"%) : Done"
 		start = end
 
 	print "Done"
 	return
 
+dval_objs = []
+QCL_objs = []
+site_objs = []
+variable_objs = []
+
 parse_small_tables()
 buffered_parse_large_table()
-print "\nParsing Complete"
+
+print "\nMigration Complete"
 conn.close()
