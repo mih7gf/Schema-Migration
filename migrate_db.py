9c2169cb5f226ef0c4708bc097301fc37b7686d9
mih7gf <mih7gf@virginia.edu>
2017-07-07 17:29:10 -0400

    Migration Started.

--- migrate_db.py
+++ migrate_db.py
@@ -2,17 +2,16 @@ import sqlite3
 from ODM1_table_objects import *
 
 
-# dval_objs = []
-# QCL_objs = []
-# site_objs = []
-# variable_objs = []
+sqlite_file2 = '../ODM2_hampt_rd_data.sqlite'
+conn2 = sqlite3.connect(sqlite_file2)
+c2 = conn2.cursor()	
 
 
 def migrate_datavalue(dval):
+	
 	return
 
 def migrate_QCL(QCL):
-	print "migrated",QCL.row
 	return
 
 def migrate_site(site):
@@ -20,5 +19,8 @@ def migrate_site(site):
 	return
 
 def migrate_variable(var):
+
 	return
 
+
+conn.close()
