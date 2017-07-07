8bd005210d61c052983fcd766a002505d4bc58b8
mih7gf <mih7gf@virginia.edu>
2017-07-07 17:26:34 -0400

    Parser complete and Migration started.

---
+++ migrate_db.py
@@ -0,0 +1,24 @@
+import sqlite3
+from ODM1_table_objects import *
+
+
+# dval_objs = []
+# QCL_objs = []
+# site_objs = []
+# variable_objs = []
+
+
+def migrate_datavalue(dval):
+	return
+
+def migrate_QCL(QCL):
+	print "migrated",QCL.row
+	return
+
+def migrate_site(site):
+
+	return
+
+def migrate_variable(var):
+	return
+
