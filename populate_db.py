c8d89f9336f829204af00e324da1a6f6a0e5af4f
mih7gf <mih7gf@virginia.edu>
2017-07-07 14:51:03 -0400

    filename chage and new file

---
+++ populate_db.py
@@ -0,0 +1,21 @@
+import sqlite3
+from ODM1_datavalue_objects import *
+from parse_db import *
+
+# dval_objs = []
+# QCL_objs = []
+# site_objs = []
+# variable_objs = []
+
+
+def handle_datavalue_data():
+	return
+
+def handle_QCL_data():
+	return
+
+def handle_sites_data():
+	return
+
+def handle_variable_data():
+	return
