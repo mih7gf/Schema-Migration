2f4967a78360347ea835ae72ddddec821b7b5fde
mih7gf <mih7gf@virginia.edu>
2017-07-11 11:11:59 -0400

    Sites migration complete.

--- migrate_db.py
+++ migrate_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 from ODM1_table_objects import *
-
+from ODM2_table_objects import *
 
 sqlite_file2 = '../ODM2_hampt_rd_data.sqlite'
 conn2 = sqlite3.connect(sqlite_file2)
@@ -12,9 +12,15 @@ def migrate_datavalue(dval):
 	return
 
 def migrate_QCL(QCL):
+	q = Core_ProcessingLevel(QCL)
+	c2.execute("INSERT INTO ProcessingLevels VALUES ({pid},'{cd}',{df},'{ex}')".format(pid=q.ProcessingLevelID, cd=q.ProcessingLevelCode, df=q.Definition, ex=q.Explanation))
+	
 	return
 
 def migrate_site(site):
+	s = SamplingFeatures_Sites(site)
+	c2.execute("INSERT INTO Sites VALUES ({sfid},'{cv}',{lt},'{ln}', {srid})".format(sfid=s.SamplingFeatureID, cv=s.SiteTypeCV, lt=s.Latitude, ln=s.Longitude, ssrid=s.SpatialReferenceID))
+
 
 	return
 
@@ -23,4 +29,3 @@ def migrate_variable(var):
 	return
 
 
-conn.close()
