453ff5fa89ec5d723de848e0b784074599ec299c
mih7gf <mih7gf@virginia.edu>
2017-07-12 17:49:56 -0400

    Starting Action

--- migrate_db.py
+++ migrate_db.py
@@ -20,14 +20,32 @@ def migrate_QCL(QCL):
 def migrate_site(site):
 	s1 = SamplingFeatures_Site(site)
 	s2 = SamplingFeature(site)
-	#-c2.execute("INSERT INTO Sites VALUES ({sfid},'{cv}',{lt},'{ln}', {srid})".format(sfid=s1.SamplingFeatureID, cv=s1.SiteTypeCV, lt=s1.Latitude, ln=s1.Longitude, srid=s1.SpatialReferenceID))
+	# s3 = FeatureAction(site)
+	c2.execute("INSERT INTO Sites VALUES ({sfid},'{cv}',{lt},'{ln}', {srid})".format(sfid=s1.SamplingFeatureID, cv=s1.SiteTypeCV, lt=s1.Latitude, ln=s1.Longitude, srid=s1.SpatialReferenceID))
 	c2.execute("INSERT INTO SamplingFeatures VALUES ({id}, '{uid}', '{tp}', '{cd}', '{nm}', '{ds}', '{gt}', '{gm}', '{gmw}', '{em}', '{ed}')".format(id=s2.SamplingFeatureID, uid=s2.SamplingFeatureUUID, tp=s2.SamplingFeatureTypeCV, cd=s2.SamplingFeatureCode, nm=s2.SamplingFeatureName, ds=s2.SamplingFeatureDescription, gt=s2.FeatureGeometry, gm=s2.FeatureGeometry ,gmw=s2.FeatureGeometryWKT, em=s2.Elevation_m, ed=s2.ElevationDatumCV))
+	migrate_Action(site)
+	migrate_FeatureAction()
+
+	c2.execute("INSERT INTO Actions VALUES ()"format())
+	c2.execute("INSERT INTO FeatureActions VALUES ()"format())
 	conn2.commit()
 	return
 
 def migrate_variable(var):
 	v = Core_Variable(var)
 	c2.execute("INSERT INTO Variables VALUES ({id}, '{cv}', '{c}', '{n}', '{d}', '{s}', {ndv})".format(id=v.VariableID, cv=v.VariableTypeCV, c=v.VariableCode, n=v.VariableNameCV, d=v.VariableDefinition, s=v.SpeciationDV, ndv=v.NoDataValue))
+	conn2.commit()
+	return
+
+def migrate_Action():
+	a = Core_Action()
+
+	conn2.commit()
+	return
+
+def migrate_FeatureAction():
+	fa = Core_FeatureAction()
+
 	conn2.commit()
 	return
 
@@ -66,10 +84,13 @@ def Method_setup():
 	m1 = Core_Method(1)
 	m2 = Core_Method(2)
 	m3 = Core_Method(3)
-	m4 = Core_Method(4)
-	methods = [m1, m2, m3, m4]
+	# m4 = Core_Method(4)
+	methods = [m1, m2, m3]
 	for method in methods:
 		c2.execute("INSERT INTO Methods VALUES ({id}, '{cv}', '{cd}', '{nm}', '{ds}', '{ln}', {org})".format(id=method.MethodID, cv=method.MethodTypeCV, cd=method.MethodCode, nm=method.MethodName, ds=method.MethodDescription, org=method.OrganizationID, ln=method.MethodLink))
 	conn2.commit()
-
 	# "raw QC level 0 data"
+
+
+# SELECT * FROM  datavalues WHERE SiteID=2 LIMIT 1;
+# SELECT * FROM  datavalues WHERE ValueID=1;
